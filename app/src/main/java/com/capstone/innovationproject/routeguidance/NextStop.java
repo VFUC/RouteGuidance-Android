2fc9630b9458de41e3cbb7b99cfa35ec4bd78b64
Tomi <ot_tomi@hotmail.com>
2016-01-28 14:29:15 +0200

    Welcome screen started

---
+++ app/src/main/java/com/capstone/innovationproject/routeguidance/NextStop.java
@@ -0,0 +1,13 @@
+package com.capstone.innovationproject.routeguidance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NextStop extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next_stop);
+    }
+}
