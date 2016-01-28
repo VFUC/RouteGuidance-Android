dab4ef84e3b6c3de884b7278b91e0416cc68ffd2
Tomi <ot_tomi@hotmail.com>
2016-01-28 14:09:15 +0200

    Welcome screen started

--- app/src/main/java/com/capstone/innovationproject/routeguidance/MainActivity.java
+++ app/src/main/java/com/capstone/innovationproject/routeguidance/MainActivity.java
@@ -4,10 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//comment
+
+    //comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
 }
