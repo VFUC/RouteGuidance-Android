6230ba2da12a4e358e0d37f95bdc96d588f96d96
Tomi <ot_tomi@hotmail.com>
2016-01-29 08:54:11 +0200

    Modified button functionality

--- app/src/main/java/com/capstone/innovationproject/routeguidance/MainActivity.java
+++ app/src/main/java/com/capstone/innovationproject/routeguidance/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,9 +14,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void sendMessage(View view){
-        Intent intent = new Intent(getApplicationContext(), NextStop.class);
-        startActivity(intent);
+    public void onClick(View v){
+        Intent i;
+        switch (v.getId()){
+            case R.id.button1Main:
+                i = new Intent(MainActivity.this, NextStop.class);
+                startActivity(i);
+                break;
+            case  R.id.button2Main:
+                //button2Main action here
+                break;
+            case  R.id.button3Main:
+                //button3Main action here
+                break;
+            case  R.id.button4Main:
+                //button4Main action here
+                break;
+            default:
+                return;
+        }
     }
 
 }
