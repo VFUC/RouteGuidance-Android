760a6040a25970b29db012e8ea9ff5c3c2d171c0
Jussi <jussi.vahamaki@edu.turkuamk.fi>
2016-02-10 09:49:34 +0200

    Added bus selection screen (empty)

---
+++ app/src/main/java/com/capstone/innovationproject/routeguidance/SelectBus.java
@@ -0,0 +1,12 @@
+package com.capstone.innovationproject.routeguidance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectBus extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_bus);
+    }
+}
