52e7049cad2cdef568adb6e2bf7562bc505ee12d
Lauluuk <lauri.luukkonen@edu.turkuamk.fi>
2016-02-19 11:20:14 +0200

    Added stops Json parsing.

--- app/src/main/java/com/capstone/innovationproject/routeguidance/SelectDestination.java
+++ app/src/main/java/com/capstone/innovationproject/routeguidance/SelectDestination.java
@@ -1,6 +1,5 @@
 package com.capstone.innovationproject.routeguidance;
 
-import android.location.Location;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,12 +9,15 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class SelectDestination extends AppCompatActivity implements AsyncResponse {
+    ArrayList<Row> stops = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +31,53 @@ public class SelectDestination extends AppCompatActivity implements AsyncRespons
     }
 
     public void processFinish(String output) {
+        String key;
+        String busStop;
 
-        //Tänne koodi millä luetaan data (output)
 
+        int i=0;
+
+        try {
+            JSONObject jsonRootObject = new JSONObject(output);
+
+            Iterator<String> iter = jsonRootObject.keys();
+            while (iter.hasNext()) {
+                key = iter.next();
+                JSONObject jsonBusStop = jsonRootObject.optJSONObject(key);
+                busStop = jsonBusStop.optString("stop_name");
+                stops.add(new Row(key, busStop));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();}
+        Collections.sort(stops, new Comparator<Row>() {
+            public int compare(Row v1, Row v2) {
+                return v1.getStopName().compareTo(v2.getStopName());
+            }
+        });
+    }
+
+    public class Row {
+        public String stopNumber;
+        public String stopName;
+
+        public Row(String stopNumber, String stopName) {
+            this.stopNumber = stopNumber;
+            this.stopName = stopName;
+
+        }
+
+        public String getStopNumber() {
+            return stopNumber;
+        }
+
+        public String getStopName() { return stopName; }
+
+        /*@Override
+        public int compareTo(Row compareStops) {
+            String compareStopNames=((Row)compareStops).getStopName();
+            return this.stopName-compareStopNames;
+            //return o1.get(0).compareTo(o2.get(0));
+        }*/
     }
 
     public void updateData() {
@@ -39,7 +85,7 @@ public class SelectDestination extends AppCompatActivity implements AsyncRespons
         timer.scheduleAtFixedRate(new TimerTask() {
             public void run() {
                 try {
-                    GetData data = new GetData(new URL("http://data.foli.fi/doc/siri/v0/sm"));
+                    GetData data = new GetData(new URL("http://data.foli.fi/siri/sm"));
                     data.delegate = SelectDestination.this;
                     data.execute();
                 } catch (Exception e) {
