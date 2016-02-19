3ab962ba45b7b068d73f50097ac1a50e40f5484b
Jussi <jussi.vahamaki@edu.turkuamk.fi>
2016-02-19 10:26:00 +0200

    SelectDestination should now read data from the internet

--- app/src/main/java/com/capstone/innovationproject/routeguidance/SelectBus.java
+++ app/src/main/java/com/capstone/innovationproject/routeguidance/SelectBus.java
@@ -14,7 +14,6 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
--- app/src/main/java/com/capstone/innovationproject/routeguidance/SelectDestination.java
+++ app/src/main/java/com/capstone/innovationproject/routeguidance/SelectDestination.java
@@ -1,17 +1,53 @@
 package com.capstone.innovationproject.routeguidance;
 
+import android.location.Location;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
-public class SelectDestination extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URL;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SelectDestination extends AppCompatActivity implements AsyncResponse {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_destination);
-
+        updateData();
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+
     }
+
+    public void processFinish(String output) {
+
+        //Tänne koodi millä luetaan data (output)
+
+    }
+
+    public void updateData() {
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            public void run() {
+                try {
+                    GetData data = new GetData(new URL("http://data.foli.fi/doc/siri/v0/sm"));
+                    data.delegate = SelectDestination.this;
+                    data.execute();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, 0, 10000);
+    }
+
 }
